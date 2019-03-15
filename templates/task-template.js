7f5669e0a58961c2796babac3f238027f65f1da5
Milan Radojevic <mikister2012@gmail.com>
2019-03-15 23:56:42 +0100

    Make tasks checkable

--- templates/task-template.js
+++ templates/task-template.js
@@ -1,7 +1,13 @@
 const TaskTemplate = `
 <div class="task" :class="{ 'task--completed': isChecked }">
 
-    <span :class="{['task__checkbox task--importance-' + importance]: true, ' task--completed': isChecked }">
+    <span 
+        :class="{
+            ['task__checkbox task--importance-' + importance]: true, 
+            ' task--completed': isChecked 
+        }"
+        v-on:click="isChecked = !isChecked"
+    >
         <i v-if="isChecked" class="fas fa-check-square"></i>
         <i v-else class="far fa-square"></i>
     </span>
