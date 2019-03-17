da3c3a85ba403301d39e9a9825c32c296533230c
Milan Radojevic <mikister2012@gmail.com>
2019-03-17 09:49:19 +0100

    Task styling

--- templates/task-template.js
+++ templates/task-template.js
@@ -1,18 +1,15 @@
 const TaskTemplate = `
-<div class="task" :class="{ 'task--completed': isChecked }">
+<div class="task">
 
     <span 
-        :class="{
-            ['task__checkbox task--importance-' + importance]: true, 
-            ' task--completed': isChecked 
-        }"
+        :class="['task__checkbox task--importance-' + importance]"
         v-on:click="isChecked = !isChecked"
     >
         <i v-if="isChecked" class="fas fa-check-square"></i>
         <i v-else class="far fa-square"></i>
     </span>
 
-    <span class="task__name">{{ name }}</span>
+    <span :class="{ 'task__name': true, 'task--completed': isChecked }">{{ name }}</span>
 
     <span>{{ dueDate }}</span>
 </div>`
